d1844335ea36eab695706302bde2da50653dee2d
chezzandyto <andres_toapanta@hotmail.com>
2023-09-18 20:22:14 -0500

    Buttons screen

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp.router(
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 9).getTheme(),
+      theme: AppTheme(selectedColor: 3).getTheme(),
     );
   }
 }
